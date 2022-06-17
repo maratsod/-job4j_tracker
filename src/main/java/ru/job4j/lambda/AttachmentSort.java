6e70a5b63d2f047e76aa572b80f6e8d574df2a62
maratsodnopov <sodnopov@list.ru>
2022-06-17 13:58:05 +0800

    2.1. Лямбда блок [#249208 #34784]

---
+++ src/main/java/ru/job4j/lambda/AttachmentSort.java
@@ -0,0 +1,30 @@
+package ru.job4j.lambda;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class AttachmentSort {
+    public static void main(String[] args) {
+        List<Attachment> attachments = Arrays.asList(
+                new Attachment("image 1", 100),
+                new Attachment("image 3", 34),
+                new Attachment("image 2", 12)
+        );
+        Comparator<Attachment> comparator = (left, right)
+                -> Integer.compare(left.getSize(), right.getSize());
+        attachments.sort(comparator);
+        System.out.println(attachments);
+
+        Comparator<Attachment> nameComparator =  (left, right)
+                -> left.getName().compareTo(right.getName());
+        attachments.sort(nameComparator);
+        System.out.println(attachments);
+
+        Comparator<String> cmpText =  (left, right)
+                -> left.compareTo(right);
+        Comparator<String> cmpDescSize =  (left, right)
+                -> Integer.compare(right.length(), left.length());
+    }
+}
+
